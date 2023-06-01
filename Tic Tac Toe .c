bf2c88217a49221e99eaa05dffb772e353ab9426
littleXinYu <134776194+littleXinYu@users.noreply.github.com>
2023-06-01 22:46:37 +0800

    存在bug，明天进行修复

---
+++ Tic Tac Toe .c
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "game.h"
+
+void game()
+{
+	//生成3X3棋盘
+	char board[ROW][COL] ;
+
+	//初始化棋盘
+	CleanBoard( board, ROW, COL );
+
+	//打印棋盘
+	PrintBoard( board, ROW, COL );
+
+	
+	while (1)
+	{
+		//玩家下棋
+		PlayerMove(board, ROW, COL);
+		PrintBoard(board, ROW, COL);
+		
+		//电脑下棋
+		AIMove(board, ROW, COL);
+		PrintBoard(board, ROW, COL);
+
+		//判断是否获胜
+		char whether = GameOver(board, ROW, COL);
+		if (whether == '*')
+		{
+			printf("玩家获胜！\n");
+			break;
+		}
+		else if (whether == '#')
+		{
+			printf("AI获胜！\n");
+			break;
+		}
+		else if (whether == 'D')
+		{
+			printf("双方平局！\n");
+			break;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	printf("本局游戏结束！\n\n\n");
+}
+
+void menu()
+{
+	printf("—————————————\n");
+	printf("————1.开始游戏————\n");
+	printf("————0.退出游戏————\n");
+	printf("—————————————\n");
+}
+
+int main()
+{
+	int input = 1;
+	srand((unsigned int)time(NULL));
+
+	do
+	{
+		menu();
+		printf("请选择：");
+		scanf("%d", &input);
+		if (input == 1)
+		{
+			printf("下面我们开始游戏！\n");
+			game();
+		}
+		else if (input == 0)
+		{
+			printf("已成功退出游戏！\n");
+		}
+		else
+		{
+			printf("输入非法数字，请重新选择！\n");
+		}
+	} while (input);
+	
+
+	return 0;
+}
