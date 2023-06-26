54d96a51bcfe2706452ffcb1ea8d357e23d8ef9a
littleXinYu <134776194+littleXinYu@users.noreply.github.com>
2023-06-26 16:48:57 +0800

    Add files via upload

--- bubble_sort_1.c
+++ bubble_sort_1.c
@@ -69,3 +69,60 @@
 //	print_arr(arr, sz);
 //	return 0;
 //}
+
+//多种类型使用的冒泡排序
+
+void swap(char* buf1, char* buf2, int width)
+{
+	int i = 0;
+	for (i = 0; i < width; i++)
+	{
+		char tmp = *buf1;
+		*buf1 = *buf2;
+		*buf2 = tmp;
+		*buf1++;
+		*buf2++;
+	}
+}
+void bubble_sort(void* base,
+				int sz,
+				int width,
+				int (*cmp)(const void*e1, const void*e2)
+				)
+{
+	int i = 0;
+	int j = 0;
+		
+	for (i = 0; i < sz - 1; i++)
+	{
+		for (j = 0; j < sz- i - 1; j++)
+		{
+			if (cmp( (char*)base + j*width, (char*)base + (j+1)*width) > 0)
+			{
+				swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
+			}
+		}
+	}
+}
+
+int cmp_int(const void* p1, const void* p2)
+{
+	return (*(int*)p1 - *(int*)p2);
+}
+
+int cmp_double(const void* p1, const void* p2)
+{
+	return (*(double*)p1 - *(double*)p2);
+}
+
+int main()
+{
+	int sz = 0;
+	int width = 0;
+	double arr[9] = { 1.0, 3.2, 5.4, 7.8, 2.1, 4.6, 6.7, 8.9 };//①只需要改要排序的类型
+	sz = sizeof(arr) / sizeof(arr[0]);
+	width = sizeof(arr[0]);
+	bubble_sort(arr, sz, width, cmp_double);//②只需要改最后一个
+	//print_sort(arr);//打印不同类型数组这一块还是需要学习
+	return 0;
+}
